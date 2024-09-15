b0c96240a996ae02ebfed32b8916f4efb3e91bc1
bandhandut19 <bondon21081@gmail.com>
2024-09-15 20:07:13 +0600

    banner setup done

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,19 @@
 // import OfferSection from "../OfferSection/OfferSection";
 
 import Banner from "../ui/Banner";
+import CallToAction from "../ui/CallToAction";
 
 const Home = () => {
   return (
-    <div>
+    <div className="">
       {/* <OfferSection></OfferSection> */}
-      <Banner></Banner>
+      <div className="mb-10">
+        <Banner></Banner>
+      </div>
+
+      <div className="mx-auto w-4/5 border-4 border-l-0 border-[#1A4862] border-r-0 border-b-0 rounded-md">
+        <CallToAction></CallToAction>
+      </div>
     </div>
   );
 };
--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -1,17 +1,31 @@
 import ReactPlayer from "react-player";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
-    <div>
-      <ReactPlayer
-        url="/coverr-bikers-driving-away-down-a-path-9981-1080p.mp4"
-        playing
-        loop
-        muted
-        width="auto"
-        height="auto"
-        controls={false}
-      />
+    <div className="">
+      <div className="overflow-hidden z-1 h-auto">
+        <ReactPlayer
+          url="/coverr-bikers-driving-away-down-a-path-9981-1080p.mp4"
+          playing
+          loop
+          muted
+          width="auto"
+          height="auto"
+          controls={false}
+        />
+      </div>
+
+      <div className=" bg-black z-2 top-[4rem] lg:top-[4.7rem] w-full absolute py-8 bg-opacity-40 lg:bg-opacity-15">
+        <h1 className="text-center">
+          <Link
+            to={"/allbikes"}
+            className="bg-[#D7DFA3] bg-opacity-90 lg:text-xl text-sm animate-pulse hover:bg-[#1A4862] hover:text-[#D7DFA3] text-[#1A4862] font-extrabold py-4 px-4 lg:py-4 lg:px-10"
+          >
+            Rent a Bike Now !
+          </Link>
+        </h1>
+      </div>
     </div>
   );
 };
