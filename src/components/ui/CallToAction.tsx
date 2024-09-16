346fbc3da4a539ae4f697efb0523d49aaceb8519
bandhandut19 <bondon21081@gmail.com>
2024-09-16 13:39:06 +0600

    clear filter button setup done

--- src/components/ui/CallToAction.tsx
+++ src/components/ui/CallToAction.tsx
@@ -9,12 +9,19 @@ const CallToAction = () => {
     setSearchName(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchName("");
+  };
   return (
     <div>
       <h1 className="text-4xl font-bold lg:text-[#1A4862] text-[#D7DFA3] mb-5 mt-3 text-center">
         Search Your Aqauinted Bike
       </h1>
-      <form className="grid lg:grid-cols-2 grid-cols-1 lg:gap-5 gap-5 text-center lg:text-left items-center justify-center py-2 px-2">
+      <form
+        onSubmit={handleSubmit}
+        className="grid lg:grid-cols-2 grid-cols-1 lg:gap-5 gap-5 text-center lg:text-left items-center justify-center py-2 px-2"
+      >
         <div>
           <Input
             className="bg-[#1A4862] bg-opacity-80 text-opacity-100 font-semibold text-[#D7DFA3] placeholder:text-opacity-80 placeholder:text-[#D7DFA3] placeholder:font-bold"
@@ -30,7 +37,9 @@ const CallToAction = () => {
           <Input></Input>
         </div>
         <div>
-          <ButtonDefault buttontext="Clear Filters"></ButtonDefault>
+          <button type="submit">
+            <ButtonDefault buttontext="Clear Filters"></ButtonDefault>
+          </button>
         </div>
       </form>
     </div>
