d85c01de3e92df95b396a36c2787013572924a55
bandhandut19 <bondon21081@gmail.com>
2024-09-25 18:52:14 +0600

    implemented redux persist for user persistance

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
+import { persistor, store } from "./redux/store.ts";
 import { RouterProvider } from "react-router-dom";
 import rootRoute from "./router/rootRoute.tsx";
 import { Toaster } from "sonner";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Toaster position="top-center" />
-      <RouterProvider router={rootRoute}></RouterProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <Toaster position="top-center" />
+        <RouterProvider router={rootRoute}></RouterProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
